5b1ac4fb1b5b88ed8440f546c82077cd418a6b1b
mardolfi <mardolfi1607@gmail.com>
2023-02-10 21:41:01 -0300

    init deployment

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'ml160707',
-    database: 'chat_app',
+    host: process.env.MYSQL_ADDON_HOST || 'localhost',
+    username: process.env.MYSQL_ADDON_USER || 'root',
+    password: process.env.MYSQL_ADDON_PASSWORD || 'ml160707',
+    database: process.env.MYSQL_ADDON_DB || 'chat_app',
     define: {
         timestamps: true,
         underscored: true,
